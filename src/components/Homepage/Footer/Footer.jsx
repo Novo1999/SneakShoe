d05927ba80a3304cc0158b57f455fc44b8142327
Novodip Mondal <neptune.navo@gmail.com>
2023-08-06 01:11:22 +0600

    landing done

--- src/components/Homepage/Footer/Footer.jsx
+++ src/components/Homepage/Footer/Footer.jsx
@@ -1,10 +1,14 @@
-import './Footer.scss';
+import "./Footer.scss";
 function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+  return (
+    <section className="footer">
+      <p>&copy; 2023 SneakShoe. Powered by SneakShoe Store</p>
+      <a href="https://github.com/Novo1999">
+        <img className="git" src="/images/github.png" alt="github" />
+      </a>
+      <img src="/images/payment-icons.png" alt="payment" />
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
